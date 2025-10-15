d00a7f0a3251532445fcc8f7eb6d72879ee1a56b
DianaEspinal <dianicee2002@gmail.com>
2025-10-14 21:35:51 -0600

    Agregando taller 1

--- app/views/dia2.php
+++ app/views/dia2.php
@@ -30,8 +30,24 @@
             <h2>Taller #1 - Toma de decisiones estratégicas basadas en datos</h2>
             <h3>Impartido por: MSc. Enrique Cortez</h3>
             <div class="contenido">
-                <img src="" alt="">
+                <img src="/images/taller1.1.jpg" alt="">
+                <img src="/images/taller1.PNG" alt="">
+                <img src="/images/taller1.2.jpg" alt="">
+                <p>Durante este taller, el MSc. Enrique nos fomentó el pensamiento crítico y el valor de tomar decisiones fundamentadas en datos previamente evaluados. 
+                A través de una dinámica práctica, nos presentó un conjunto de datos completamente desordenados, simulando una situación real en la que la información no está estructurada ni categorizada.
+                Con paciencia y método, procedió a organizar esos datos, aplicando filtros y segmentaciones por categorías relevantes, lo que permitió visualizar patrones y tendencias de manera clara.
+                Este proceso no solo demostró la importancia de la limpieza y estructuración de datos, sino que también evidenció cómo estas acciones pueden facilitar la toma de decisiones estratégicas. 
+                Por ejemplo, en el caso de un propietario de negocio, contar con datos organizados le permite identificar qué categoría de productos o servicios tiene mayor movimiento dentro de su mercado, 
+                y así tomar decisiones informadas sobre inversión, promoción o mejora operativa.
+                cuando se fundamentan en datos concretos, evaluados y contextualizados, se obtiene una visión más precisa del entorno, lo que permite elegir el camino más adecuado o detectar áreas que requieren ajustes.
+                Este enfoque analítico es esencial en el mundo actual, donde la información es 
+                abundante pero solo se convierte en conocimiento útil cuando se interpreta correctamente.
+            </p>
             </div>
+            <video controls class="video-vertical">
+                <source src="/videos/taller1.mp4" type="video/mp4">
+                Tu navegador no soporta el video.
+            </video>
         </section>
     </main>
 </body>
