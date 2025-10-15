2fcfcdbe0c66e4a606a77be445e0e54968736711
DianaEspinal <dianicee2002@gmail.com>
2025-10-14 22:06:58 -0600

    Agregando taller 3, ademas subiendo audios del taller.

--- app/views/dia2.php
+++ app/views/dia2.php
@@ -1,15 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/css/style.css">
-    
+
     <link href="https://fonts.googleapis.com/css2?family=Cuprum&display=swap" rel="stylesheet">
     <title>Dia 2- Semana De Sistemas</title>
 </head>
+
 <body>
-   <header>
+    <header>
         <h1>Semana de Sistemas 2025</h1>
         <nav>
             <a href="/">Inicio</a>
@@ -33,16 +35,16 @@
                 <img src="/images/taller1.1.jpg" alt="">
                 <img src="/images/taller1.PNG" alt="">
                 <img src="/images/taller1.2.jpg" alt="">
-                <p>Durante este taller, el MSc. Enrique nos fomentó el pensamiento crítico y el valor de tomar decisiones fundamentadas en datos previamente evaluados. 
-                A través de una dinámica práctica, nos presentó un conjunto de datos completamente desordenados, simulando una situación real en la que la información no está estructurada ni categorizada.
-                Con paciencia y método, procedió a organizar esos datos, aplicando filtros y segmentaciones por categorías relevantes, lo que permitió visualizar patrones y tendencias de manera clara.
-                Este proceso no solo demostró la importancia de la limpieza y estructuración de datos, sino que también evidenció cómo estas acciones pueden facilitar la toma de decisiones estratégicas. 
-                Por ejemplo, en el caso de un propietario de negocio, contar con datos organizados le permite identificar qué categoría de productos o servicios tiene mayor movimiento dentro de su mercado, 
-                y así tomar decisiones informadas sobre inversión, promoción o mejora operativa.
-                cuando se fundamentan en datos concretos, evaluados y contextualizados, se obtiene una visión más precisa del entorno, lo que permite elegir el camino más adecuado o detectar áreas que requieren ajustes.
-                Este enfoque analítico es esencial en el mundo actual, donde la información es 
-                abundante pero solo se convierte en conocimiento útil cuando se interpreta correctamente.
-            </p>
+                <p>Durante este taller, el MSc. Enrique nos fomentó el pensamiento crítico y el valor de tomar decisiones fundamentadas en datos previamente evaluados.
+                    A través de una dinámica práctica, nos presentó un conjunto de datos completamente desordenados, simulando una situación real en la que la información no está estructurada ni categorizada.
+                    Con paciencia y método, procedió a organizar esos datos, aplicando filtros y segmentaciones por categorías relevantes, lo que permitió visualizar patrones y tendencias de manera clara.
+                    Este proceso no solo demostró la importancia de la limpieza y estructuración de datos, sino que también evidenció cómo estas acciones pueden facilitar la toma de decisiones estratégicas.
+                    Por ejemplo, en el caso de un propietario de negocio, contar con datos organizados le permite identificar qué categoría de productos o servicios tiene mayor movimiento dentro de su mercado,
+                    y así tomar decisiones informadas sobre inversión, promoción o mejora operativa.
+                    cuando se fundamentan en datos concretos, evaluados y contextualizados, se obtiene una visión más precisa del entorno, lo que permite elegir el camino más adecuado o detectar áreas que requieren ajustes.
+                    Este enfoque analítico es esencial en el mundo actual, donde la información es
+                    abundante pero solo se convierte en conocimiento útil cuando se interpreta correctamente.
+                </p>
             </div>
             <video controls class="video-vertical">
                 <source src="/videos/taller1.mp4" type="video/mp4">
@@ -51,23 +53,46 @@
         </section>
         <section class="ponencia">
             <h2>Taller #2 - Construyendo un CRUD en minutos con Laravel y Filament.</h2>
-            <h3>Impartido por:  Ing. Gabriel Castillo</h3>
+            <h3>Impartido por: Ing. Gabriel Castillo</h3>
             <div class="contenido">
                 <img src="/images/taller2.png" alt="">
                 <img src="/images/taller2.2.png" alt="">
                 <img src="/images/taller2.3.png" alt="">
-                <p>Durante este taller, el Ing. Gabriel nos introdujo al desarrollo moderno utilizando Laravel y Filament, dos herramientas altamente eficientes para la creación de 
-                    aplicaciones web escalables. Comenzó explicando la documentación oficial de ambos frameworks, destacando su claridad, 
-                    estructura modular y el enfoque práctico que ofrecen. nos guió paso a paso en el proceso de instalación de Laravel y Filament, resaltando cómo estas herramientas 
-                    permiten ahorrar tiempo significativo al momento de desarrollar proyectos complejos. Gracias a sus componentes integrados y su arquitectura bien definida, es posible construir interfaces 
+                <p>Durante este taller, el Ing. Gabriel nos introdujo al desarrollo moderno utilizando Laravel y Filament, dos herramientas altamente eficientes para la creación de
+                    aplicaciones web escalables. Comenzó explicando la documentación oficial de ambos frameworks, destacando su claridad,
+                    estructura modular y el enfoque práctico que ofrecen. nos guió paso a paso en el proceso de instalación de Laravel y Filament, resaltando cómo estas herramientas
+                    permiten ahorrar tiempo significativo al momento de desarrollar proyectos complejos. Gracias a sus componentes integrados y su arquitectura bien definida, es posible construir interfaces
                     administrativas, formularios y sistemas de gestión de datos con rapidez y eficacia.
-                    Uno de los puntos clave que el ingeniero enfatizó fue la importancia de contar con una conexión sólida y bien configurada a la base de datos. Esta base es esencial para que Laravel y Filament funcionen correctamente, ya que gran parte de su 
+                    Uno de los puntos clave que el ingeniero enfatizó fue la importancia de contar con una conexión sólida y bien configurada a la base de datos. Esta base es esencial para que Laravel y Filament funcionen correctamente, ya que gran parte de su
                     funcionalidad depende de la interacción fluida con los datos almacenados.
-                   Como demostración práctica, el Ing. Gabriel desarrolló en tiempo real un proyecto funcional. 
-                   Este proyecto permitía agregar productos con atributos como nombre, precio, fotografía, tallas y otros detalles relevantes. La rapidez con la que se logró implementar esta solución dejó en evidencia el poder de Laravel y Filament para acelerar el desarrollo.
+                    Como demostración práctica, el Ing. Gabriel desarrolló en tiempo real un proyecto funcional.
+                    Este proyecto permitía agregar productos con atributos como nombre, precio, fotografía, tallas y otros detalles relevantes. La rapidez con la que se logró implementar esta solución dejó en evidencia el poder de Laravel y Filament para acelerar el desarrollo.
                 </p>
             </div>
         </section>
+        <section class="ponencia">
+            <h2>Taller #3 - A Multimodal – Creando el futuro visual e inteligente con n8n.</h2>
+            <h3>Impartido por: MSc. José Salmerón</h3>
+            <div class="contenido">
+                <img src="/images/taller3.png" alt="">
+                <p>El protagonista del taller n8n, es una herramienta de automatización de flujos de trabajo que permite conectar múltiples servicios, aplicaciones
+                    y procesos sin necesidad de escribir código complejo. El MSc.Jose nos mostro cómo n8n puede integrarse con plataformas como Canva, Google Sheets, APIs externas y servicios
+                    de IA para crear flujos automatizados que ahorran tiempo, reducen errores y potencian la creatividad.
+                    El futuro de la creación digital no solo es visual, sino también inteligente. Y herramientas como n8n nos permiten construir ese futuro hoy, conectando ideas, datos y
+                    creatividad en flujos automatizados que transforman la manera en que trabajamos.
+                </p>
+                <p>(Tome video pero no se grabo el meet, si no la ventana de la pantalla principal, pero grabe audios).</p>
+            </div>
+            <audio controls>
+                <source src="/audios/taller3.m4a" type="audio/mp4">
+                Tu navegador no soporta el elemento de audio.
+            </audio>
+            <audio controls>
+                <source src="/audios/taller3.1.m4a" type="audio/mp4">
+                Tu navegador no soporta el elemento de audio.
+            </audio>
+        </section>
     </main>
 </body>
+
 </html>
