119853018738bb9011ab3178580d51a9ddf74088
DianaEspinal <dianicee2002@gmail.com>
2025-10-14 21:00:53 -0600

    insertando el header y links respectivos

--- app/views/dia2.php
+++ app/views/dia2.php
@@ -3,9 +3,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/style.css">
+    
+    <link href="https://fonts.googleapis.com/css2?family=Cuprum&display=swap" rel="stylesheet">
     <title>Dia 2- Semana De Sistemas</title>
 </head>
 <body>
-    probando dia 2
+   <header>
+        <h1>Semana de Sistemas 2025</h1>
+        <nav>
+            <a href="/">Inicio</a>
+            <a href="/dia/1">Día 1</a>
+            <a href="/dia/2">Día 2</a>
+            <a href="/dia/3">Día 3</a>
+            <a href="/dia/4">Día 4</a>
+            <a href="/dia/5">Día 5</a>
+            <a href="/mi-info">Mi información</a>
+            <a href="/visitas">Visitas</a>
+        </nav>
+    </header>
 </body>
 </html>
