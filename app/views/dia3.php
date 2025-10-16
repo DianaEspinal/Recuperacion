148ca04d282e6292ef681fc096b38c0a28d73304
DianaEspinal <dianicee2002@gmail.com>
2025-10-15 21:47:30 -0600

    agregando dia 3 y configurando el esqueleto del dia 4

---
+++ app/views/dia3.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/style.css">
+
+    <link href="https://fonts.googleapis.com/css2?family=Cuprum&display=swap" rel="stylesheet">
+    
+    <title>Dia3 - Semana de Sistemas.</title>
+</head>
+<body>
+     <header>
+        <h1>Semana de Sistemas 2025</h1>
+        <nav>
+            <a href="/">Inicio</a>
+            <a href="/dia/1">Día 1</a>
+            <a href="/dia/2">Día 2</a>
+            <a href="/dia/3">Día 3</a>
+            <a href="/dia/4">Día 4</a>
+            <a href="/dia/5">Día 5</a>
+            <a href="/mi-info">Mi información</a>
+            <a href="/visitas">Visitas</a>
+        </nav>
+    </header>
+    <main class="container">
+        <section class="intro">
+            <h2>Se suspendio el torneo. <br>
+                No hay nada que agregar.
+            </h2>
+        </section>
+    </main>
+</body>
+</html>
