9d8b6e46722e1fba7107107797e92bc4c9ed640d
DianaEspinal <dianicee2002@gmail.com>
2025-10-15 21:03:07 -0600

    Creando el codigo sql para crear la tabla en la bd

--- lib/database.sql
+++ lib/database.sql
@@ -1,9 +1,7 @@
-CREATE TABLE visits (
+CREATE TABLE visitas (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL,
-    email VARCHAR(100),
+    correo VARCHAR(100), NOT NULL,
     comentario TEXT,
-    ip_address VARCHAR(45),
-    user_agent TEXT,
     fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
