6cc5991d8a9e872d2aac46619309e920f9cb6498
DianaEspinal <dianicee2002@gmail.com>
2025-10-15 21:17:33 -0600

    Creando vista de formulario de visitas

---
+++ app/views/visitas.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro de Visitas.</title>
+</head>
+<body>
+    <header>
+    <h1>Semana de Sistemas 2025</h1>
+    <nav>
+      <a href="/">Inicio</a>
+      <a href="/dia/1">Día 1</a>
+      <a href="/dia/2">Día 2</a>
+      <a href="/dia/3">Día 3</a>
+      <a href="/dia/4">Día 4</a>
+      <a href="/dia/5">Día 5</a>
+      <a href="/mi-info">Mi información</a>
+      <a href="/visitas">Visitas</a>
+    </nav>
+  </header>
+</body>
+</html>
