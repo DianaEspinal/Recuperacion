7330a202ccdcbc4f64693f30f3658b149500e477
DianaEspinal <dianicee2002@gmail.com>
2025-10-16 21:47:47 -0600

    Dia 4 completado

--- app/views/dia4.php
+++ app/views/dia4.php
@@ -1,16 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/css/style.css">
 
     <link href="https://fonts.googleapis.com/css2?family=Cuprum&display=swap" rel="stylesheet">
-    
+
     <title>Dia4 - Semana de Sistemas.</title>
 </head>
+
 <body>
-     <header>
+    <header>
         <h1>Semana de Sistemas 2025</h1>
         <nav>
             <a href="/">Inicio</a>
@@ -26,33 +28,135 @@
     <main class="container">
         <section class="intro">
             <h2>Día 4: Feria de Logros <br>
-               y Comelona de Pupusas.</h2>
+                y Comelona de Pupusas.</h2>
         </section>
         <section class="ponencia">
             <h2>Feria de Logros.</h2>
             <div class="contenido">
-                <img src="" alt="">
-                <img src="" alt="">
-                <p></p>
+                <h3>Proyecto: El radar de Moises.</h3>
+                <img src="/images/standMoises1.jpg" alt="">
+                <img src="/images/standMoises.jpg" alt="">
             </div>
+            <br>
+            <br>
+            <div class="contenido">
+                <h3>Proyecto: Parqueo automático con Arduino y Aplicación Móvil.</h3>
+                <img src="/images/StandParqeo.jpg" alt="">
+            </div>
+            <br>
+            <br>
+            <div class="contenido">
+                <h3>Proyecto: Juego de realidad aumentada.</h3>
+                <img src="/images/StandJuegoRealidad.jpg" alt="">
+            </div>
+            <br>
+            <br>
+            <div class="contenido">
+                <h3>Proyecto: Tienda en linea con ARCore.</h3>
+                <img src="/images/StandTiendaArcore.png" alt="">
+            </div>
+            <br>
+            <br>
+            <div class="contenido">
+                <h3>Proyecto: Sistema de comandas en tiempo real.</h3>
+                <img src="/images/standBar.jpg" alt="">
+            </div>
+            <br>
+            <br>
+            <div class="contenido">
+                <h3>Proyecto: Sistema de cajero automático.</h3>
+                <img src="/images/standCajero.jpg" alt="">
+            </div>
+            <br>
+            <br>
+            <div class="contenido">
+                <h3>Proyecto: Sistema de cajero automático C# Bank.</h3>
+                <img src="/images/standSharpBank.jpg" alt="">
+            </div>
+            <br>
+            <br>
+            <div class="contenido">
+                <h3>Proyecto: Innova rent.</h3>
+                <img src="/images/standInnovaRent.jpg" alt="">
+            </div>
+            <br>
+            <br>
+            <div class="contenido">
+                <h3>Proyecto: Blog de teoria de sistemas..</h3>
+                <img src="/images/standTSI.jpg" alt="">
+            </div>
+            <br>
+            <br>
+            <div class="contenido">
+                <h3>Proyecto: Juego de Bingo.</h3>
+                <img src="/images/standBingo.jpg" alt="">
+            </div>
+            <br>
+            <br>
+            <div class="contenido">
+                <h3>Proyecto: Sistema de gestion de citas y pagos de un CarWash.</h3>
+                <img src="/images/standCarWash.jpg" alt="">
+            </div>
+            <br>
+            <br>
+            <div class="contenido">
+                <h3>Proyecto: Biblioteca virtual CUBO.</h3>
+                <img src="/images/standCubo.jpg" alt="">
+            </div>
+            <br>
+            <br>
+            <div class="contenido">
+                <h3>Proyecto: Sistema de gestion de Farmacia.</h3>
+                <img src="/images/standFarmacia.jpg" alt="">
+            </div>
+        </section>
+        <br>
+        <br>
+        <section class="ponencia">
+            <h2>Premiación de los Stands.</h2>
+            <h3>Ganadores popularidad: Stand de TSI.
+                <br> Ganadores Feria de Logros: Radar de Moises y Biblioteca Virtual CUBO.
+            </h3>
             <video controls class="video-vertical">
-                <source src="/videos/" type="video/mp4">
+                <source src="/videos/feriadeLogroPremiacion.mp4" type="video/mp4">
                 Tu navegador no soporta el video.
             </video>
         </section>
-        
+
+        <section class="intro">
+            <h2>Fotos.</h2>
+            <img src="/images/amigosJueves.jpg" alt="">
+            <img src="/images/feriaFotoMia.jpg" alt="">
+            <img src="/images/amigos2J.jpg" alt="">
+            <img src="/images/amigaJueves.jpg" alt="">
+        </section>
+
         <section class="ponencia">
             <h2>Comelona de Pupusas.</h2>
             <div class="contenido">
-                <img src="" alt="">
-                <img src="" alt="">
-                <p></p>
+                <img src="/images/comelona.jpg" alt="">
+                <img src="/images/pupuseada2.jpg" alt="">
+                <img src="/images/pupuseada2.2.jpg" alt="">
+                <p>La comelona de pupusas comenzo a las 12:00 del mediodia.
+                    Hubieron 11 participantes, de los cuales solo habrian 2 ganadores.
+                    Dieron un tiempo de 15 minutos, y empezaban con un plato de 5 pupusas.
+                </p>
             </div>
             <video controls class="video-vertical">
-                <source src="/videos/" type="video/mp4">
+                <source src="/videos/Pupuseada.mp4" type="video/mp4">
+                Tu navegador no soporta el video.
+            </video>
+            <section class="ponencia">
+                <h2>Premiación de la Pupuseada.</h2>
+                <h3>Primer Lugar: Ing. Guandique con 16 pupusas.</h3>
+                <h3>Primer Lugar: EL estudiantes Manuel con 13 pupusas.</h3>
+            </section>
+            <video controls class="video-vertical">
+                <source src="/videos/PremiacionPupuseada.mp4" type="video/mp4">
                 Tu navegador no soporta el video.
             </video>
         </section>
     </main>
 </body>
+
 </html>
