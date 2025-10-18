231230bf140d41625600188bb57a346d403c575d
DianaEspinal <dianicee2002@gmail.com>
2025-10-17 19:45:08 -0600

    Agregando descripcion del evento.

--- app/views/layout.php
+++ app/views/layout.php
@@ -30,6 +30,14 @@
       <div class="intro-texto">
         <h2>Bienvenido al Blog Semana de Sistemas 2025</h2>
         <p>Este blog documenta todas las actividades asistidas durante la Semana de Sistemas 2025. Encontrarás diversos videos e imágenes que muestran el festejo de esta tan esperada semana.</p>
+        <p>La Semana de Sistemas es un evento, mejor dicho ya una tradición que se tiene, se realiza siempre
+          en Octubre de cada año, y se festeja de una manera organizada, creativa e innovadora a nosotros los "Futuros Ingenieros en Sistemas". Se nos brinda ponencias, talleres que nos llenan de conocimientos, y nos muestran un mundo mas 
+          allá de lo que estudiamos. Además se realizan actividades de entrenimiento, para que esta semana sea inolvidable.
+          Es organizada mediante los estudiantes que pertencen a la ASEIS, y estudiantes voluntarios, como en este año fuimos nosotros.
+          Estuvimos colaborando en la decoración, creamos las letras que representa las siglas "SDS25", y que se usaron durante el evento.
+          En fin, es una semana llena de aprendizajes, distracciones, festejo con nuestros compañeros pero sobre todo
+          esto nos hace amar más la carrera que estudiamos.
+        </p>
         <p><strong>Realizado por:</strong> Diana Estefani Espinal Cárcamo - EC22003</p>
       </div>
     </section>
